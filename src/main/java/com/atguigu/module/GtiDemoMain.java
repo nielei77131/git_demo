3ebe1637bf077ed048d005f8a15367c18e263cb8
nielei <76326498@qq.com>
2024-01-17 17:10:23 +0800

    hotfix-v1

--- src/main/java/com/atguigu/module/GtiDemoMain.java
+++ src/main/java/com/atguigu/module/GtiDemoMain.java
@@ -1,7 +1,10 @@
 package com.atguigu.module;
 
+import com.atguigu.hotfix.HotFixTest;
+
 public class GtiDemoMain {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println(HotFixTest.fix());
     }
 }
