19f0b962a57df66806765396834cf72a499cf9ac
nielei <76326498@qq.com>
2024-01-17 16:58:09 +0800

    version1

--- src/main/java/com/atguigu/module/GtiDemoMain.java
+++ src/main/java/com/atguigu/module/GtiDemoMain.java
@@ -3,6 +3,6 @@ package com.atguigu.module;
 public class GtiDemoMain {
     public static void main(String[] args) {
         System.out.println("hello world!");
-        System.out.println("hello git");
+        System.out.println("first");
     }
 }
