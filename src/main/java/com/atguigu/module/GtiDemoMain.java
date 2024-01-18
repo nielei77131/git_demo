74b59fa1387b9c2e1a24eafc5086b071aa7cca4e
nielei <76326498@qq.com>
2024-01-18 09:16:40 +0800

    hotfix-v1

--- src/main/java/com/atguigu/module/GtiDemoMain.java
+++ src/main/java/com/atguigu/module/GtiDemoMain.java
@@ -4,7 +4,7 @@ import com.atguigu.hotfix.HotFixTest;
 
 public class GtiDemoMain {
     public static void main(String[] args) {
-        System.out.println("hello world!111111111333333333333333");
+        System.out.println("hello world!111111111333333333333333;777777");
         System.out.println(HotFixTest.fix());
     }
 }
