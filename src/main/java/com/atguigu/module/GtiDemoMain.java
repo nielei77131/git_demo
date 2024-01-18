3746c1791ee4c766f9e758695c95b5d8c4644351
nielei77131 <36293125+nielei77131@users.noreply.github.com>
2024-01-18 09:10:43 +0800

    Update GtiDemoMain.java

--- src/main/java/com/atguigu/module/GtiDemoMain.java
+++ src/main/java/com/atguigu/module/GtiDemoMain.java
@@ -4,7 +4,7 @@ import com.atguigu.hotfix.HotFixTest;
 
 public class GtiDemoMain {
     public static void main(String[] args) {
-        System.out.println("hello world!1111");
+        System.out.println("hello world!111111111333333333333333");
         System.out.println(HotFixTest.fix());
     }
 }
